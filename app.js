47b136fac74e9f72cc63ddc4cd2368d5a96e7088
Hashim Nadeem <61445847+Hashimn4@users.noreply.github.com>
2022-03-18 23:17:37 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const title = document.querySelector('#title');
+const contact = document.querySelector('.contact');
+
+title.addEventListener("click", () =>  {
+    window.location.href = "index.html";
+})
+
+contact.addEventListener("click", () => {
+    window.location.href = "Contact.html"   
+})
